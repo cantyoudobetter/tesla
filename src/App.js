575ee099e93208628458805e95952c065995cbcf
michael.bordelon <bordelon@10xscale.com>
2019-01-06 10:52:56 -0600

    first

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+  componentDidMount() {
+
+
+  }
+
+
+
   render() {
     return (
       <div className="App">
@@ -26,3 +35,10 @@ class App extends Component {
 }
 
 export default App;
+
+
+curl --request POST \
+  --url 'https://owner-api.teslamotors.com/' \
+  --header 'content-type: application/json' \
+  --header 'User-Agent: test' \
+  --data '{"grant_type":"password","username": "michael.d.bordelon@gmail.com","password": "Tamu1999!","audience": "https://bordelonapp.com/", "scope": "read:sample", "client_id": "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384", "client_secret": "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3"}'
